7714bdf0405809623f96b41ec01ba4d0333ca6e3
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-12-15 17:47:52 -0800

    seed files created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comicbook.create(title: "Amazing Spider-man 1", writer: "Stan Lee", artist: "Steve Ditko",   image_url: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/562/DIG000136._SX360_CLs%7C360,508%7Ccu/cmx-cu-sash-lg.png%7C0,0,361,509%20208,356,152,152_QL80_TTD_.jpg",  publisher: "Marvel")
+Comicbook.create(title: "Flash Reborn", writer: "Geoff Johns", artist: "Ethan Van Sciver",  image_url: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/48457/DIG010147_1._SX360_QL80_TTD_.jpg",   publisher: "DC")
+Comicbook.create(title: "Darkness 1 ",  writer: "Mark Silvestri", artist: "Mark Silvestri", image_url: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/1166/ICO000034._SX312_CLs%7C312,473%7Ccu/cmx-cu-sash-lg.png%7C0,0,313,474%20160,321,152,152_QL80_TTD_.jpg",  publisher: "Image")
